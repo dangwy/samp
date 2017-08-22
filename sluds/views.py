ea2578e2f559bb7c8a05e95b735aaed0747aaa51
dangwy <dangwy@gmail.com>
2017-08-22 15:43:43 +0800

    backup

--- sluds/views.py
+++ sluds/views.py
@@ -51,6 +51,7 @@ def get_user_info(request):
     :param pk:
     :return:
     """
+
     logger.info(request.body)
     #data = JSONParser().parse(request)
     #data = serializers.serialize("xml", SomeModel.objects.all())
